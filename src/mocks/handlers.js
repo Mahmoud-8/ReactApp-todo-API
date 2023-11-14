c640e00859dd87c951563d91ea042b387702da22
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-11-14 15:56:37 +0100

    done

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,15 +1,17 @@
-import { http } from 'msw';
+import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-  http.get('https://www.themealdb.com/api/json/v1/1/random.php', (req, res, ctx) => {
+  http.get('https://www.themealdb.com/api/json/v1/1/random.php', (req, res) => {
     return res(
-      ctx.json({
-        meals: [
-          {
-            strMeal: 'Mocked Meal',
-            strMealThumb: 'mocked-image.jpg',
-          },
-        ],
+      new HttpResponse({
+        body: {
+          meals: [
+            {
+              strMeal: 'Mocked Meal',
+              strMealThumb: 'mocked-image.jpg',
+            },
+          ],
+        },
       })
     );
   }),
